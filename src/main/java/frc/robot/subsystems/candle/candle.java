6992b7990b3bd4a96b510a45a1d490c7fdd1c681
anderson brodeur <2026abrodeur@mynrsd.com>
2025-02-28 13:28:17 -0500

    Added LED_COUNT final implimetation and set CANdle to control 32 LEDs

--- src/main/java/frc/robot/subsystems/candle/candle.java
+++ src/main/java/frc/robot/subsystems/candle/candle.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.candle;
 
+import com.ctre.phoenix.CANifier.LEDChannel;
 import com.ctre.phoenix.led.Animation;
 import com.ctre.phoenix.led.CANdle;
 import com.ctre.phoenix.led.FireAnimation;
@@ -40,7 +41,7 @@ public class Candle extends SubsystemBase {
   // 2025 isn't a stated robot, prob to delete
   private RobotState lastState;
 
-  private static final int LED_COUNT = 85;
+  private static final int LED_COUNT = 32;
 
   // This is ripped from 2024, no idea what it does but it might be important so it stays
   private boolean clearAnimationFlag = false;
@@ -62,14 +63,14 @@ public class Candle extends SubsystemBase {
   // these are prob right
   // Other animations:
   // https://api.ctr-electronics.com/phoenix/release/java/com/ctre/phoenix/led/package-summary.html
-  private Animation rainbow = new RainbowAnimation(1, 0.75, 16, false, 0);
-  private Animation fire = new FireAnimation(1, 0.75, 16, 0.25, 0.125, false, 0);
+  private Animation rainbow = new RainbowAnimation(1, 0.75, LED_COUNT, false, 0);
+  private Animation fire = new FireAnimation(1, 0.75, LED_COUNT, 0.25, 0.125, false, 0);
   private Animation larsonBack =
-      new LarsonAnimation(155, 155, 155, 1, 0.75, 16, LarsonAnimation.BounceMode.Back, 3, 0);
+      new LarsonAnimation(155, 155, 155, 1, 0.75, LED_COUNT, LarsonAnimation.BounceMode.Back, 3, 0);
   private Animation larsonCenter =
-      new LarsonAnimation(155, 155, 155, 1, 0.75, 16, LarsonAnimation.BounceMode.Center, 3, 0);
+      new LarsonAnimation(155, 155, 155, 1, 0.75, LED_COUNT, LarsonAnimation.BounceMode.Center, 3, 0);
   private Animation larsonFront =
-      new LarsonAnimation(155, 155, 155, 1, 0.75, 16, LarsonAnimation.BounceMode.Front, 3, 0);
+      new LarsonAnimation(155, 155, 155, 1, 0.75, LED_COUNT, LarsonAnimation.BounceMode.Front, 3, 0);
 
   @Override
   public void periodic() {
@@ -87,10 +88,10 @@ public class Candle extends SubsystemBase {
     // manipulator was made public, problem?
     else if (superStructure.manipulator.isCoralPresent() == true
         && DriverStation.isAutonomous() == false) {
-      candleLEDs.setLEDs(0, 0, 255, 175, 0, 16);
+      candleLEDs.setLEDs(0, 0, 255, 175, 0, LED_COUNT);
       System.out.println("Coral LED State");
     } else if (DriverStation.isAutonomous() == false) {
-      candleLEDs.setLEDs(0, 0, 0, 0, 0, 16);
+      candleLEDs.setLEDs(0, 0, 0, 0, 0, LED_COUNT);
       System.out.println("Defualt LED State");
     } else {
       System.out.println("Initial LED State (Probably)");
@@ -134,6 +135,6 @@ public class Candle extends SubsystemBase {
   }
 
   public void off() {
-    candleLEDs.setLEDs(0, 0, 0, 0, 0, 16);
+    candleLEDs.setLEDs(0, 0, 0, 0, 0, LED_COUNT);
   }
 }
