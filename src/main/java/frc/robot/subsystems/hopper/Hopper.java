9edd2204a35fead32c37e4dbb0591ccc8af2c112
Eshaan <superdog1155@gmail.com>
2025-05-28 17:28:49 -0400

    asdlfk

---
+++ src/main/java/frc/robot/subsystems/hopper/Hopper.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems.hopper;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.hopper.HopperIO.HopperIOInputs;
+
+import org.littletonrobotics.junction.Logger;
+
+public class Hopper extends SubsystemBase {
+  private final HopperIO io;
+  private final HopperIOInputsAutoLogged inputs = new HopperIOInputsAutoLogged();
+
+  public Hopper() {
+    io = new HopperIOTalonFX();
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Hopper", inputs);
+  }
+
+  public void runPercentOutput(double setpointPercent) {
+    io.runPercentOutput(setpointPercent);
+  }
+
+  public Command intakeCommand() {
+    return run(() -> runPercentOutput(0.4)).finallyDo(() -> runPercentOutput(0));
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/hopper/HopperIO.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.hopper;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface HopperIO {
+
+  @AutoLog
+  public static class HopperIOInputs {
+    public boolean connected = false;
+    public double velocityRadPerSec = 0.0;
+    public double appliedVolts = 0.0;
+    public double supplyCurrentAmps = 0.0;
+    public double statorCurrentAmps = 0.0;
+    public double tempCelsius = 0.0;
+  }
+
+  public default void updateInputs(HopperIOInputs inputs) {}
+
+  public default void runPercentOutput(double percent) {}
+}
