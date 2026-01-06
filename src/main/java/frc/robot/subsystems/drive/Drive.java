5e9cdc3c24216fa2a8453b14a2a7b6a89b2defbd
Andrew Rickard <arickard@salesforce.com>
2026-01-05 21:29:33 -0500

    demo calculating and logging slip-ratio

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -68,6 +68,51 @@ public class Drive extends SubsystemBase {
               Math.hypot(TunerConstants.BackLeft.LocationX, TunerConstants.BackLeft.LocationY),
               Math.hypot(TunerConstants.BackRight.LocationX, TunerConstants.BackRight.LocationY)));
 
+  /**
+   * the method comes from 1690's <a
+   * href="https://youtu.be/N6ogT5DjGOk?feature=shared&t=1674">online software session</a> gets the
+   * skidding ratio from the latest , that can be used to determine how much the chassis is skidding
+   * the skidding ratio is defined as the ratio between the maximum and minimum magnitude of the
+   * "translational" part of the speed of the modules
+   *
+   * @param swerveStatesMeasured the swerve states measured from the modules
+   * @param swerveDriveKinematics the kinematics
+   * @return the skidding ratio, maximum/minimum, ranges from [1,INFINITY)
+   */
+  public static double getSkiddingRatio(
+      SwerveModuleState[] swerveStatesMeasured, SwerveDriveKinematics swerveDriveKinematics) {
+    final double angularVelocityOmegaMeasured =
+        swerveDriveKinematics.toChassisSpeeds(swerveStatesMeasured).omegaRadiansPerSecond;
+    final SwerveModuleState[] swerveStatesRotationalPart =
+        swerveDriveKinematics.toSwerveModuleStates(
+            new ChassisSpeeds(0, 0, angularVelocityOmegaMeasured));
+    final double[] swerveStatesTranslationalPartMagnitudes =
+        new double[swerveStatesMeasured.length];
+
+    for (int i = 0; i < swerveStatesMeasured.length; i++) {
+      final Translation2d
+          swerveStateMeasuredAsVector = convertSwerveStateToVelocityVector(swerveStatesMeasured[i]),
+          swerveStatesRotationalPartAsVector =
+              convertSwerveStateToVelocityVector(swerveStatesRotationalPart[i]),
+          swerveStatesTranslationalPartAsVector =
+              swerveStateMeasuredAsVector.minus(swerveStatesRotationalPartAsVector);
+      swerveStatesTranslationalPartMagnitudes[i] = swerveStatesTranslationalPartAsVector.getNorm();
+    }
+
+    double maximumTranslationalSpeed = 0, minimumTranslationalSpeed = Double.POSITIVE_INFINITY;
+    for (double translationalSpeed : swerveStatesTranslationalPartMagnitudes) {
+      maximumTranslationalSpeed = Math.max(maximumTranslationalSpeed, translationalSpeed);
+      minimumTranslationalSpeed = Math.min(minimumTranslationalSpeed, translationalSpeed);
+    }
+
+    return maximumTranslationalSpeed / minimumTranslationalSpeed;
+  }
+
+  private static Translation2d convertSwerveStateToVelocityVector(
+      SwerveModuleState swerveModuleState) {
+    return new Translation2d(swerveModuleState.speedMetersPerSecond, swerveModuleState.angle);
+  }
+
   // PathPlanner config constants
   private static final double ROBOT_MASS_KG = 52.5;
   private static final double ROBOT_MOI = 5.100;
@@ -158,6 +203,8 @@ public class Drive extends SubsystemBase {
     odometryLock.lock(); // Prevents odometry updates while reading data
     gyroIO.updateInputs(gyroInputs);
     Logger.processInputs("Drive/Gyro", gyroInputs);
+    Logger.recordOutput("Drive/SkiddingRatio", getSkiddingRatio(getModuleStates(), kinematics));
+
     for (var module : modules) {
       module.periodic();
     }
